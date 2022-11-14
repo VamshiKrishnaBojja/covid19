38f2e2186d7bdbd49d54b75ad12768b9f7a1b955
Vamshi Krishna Bojja <114059960+VamshiKrishnaBojja@users.noreply.github.com>
2022-11-14 21:42:31 +0530

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,2 +1,2 @@
-#rm -r /home/ubuntu/server/lastbuild
-#mv /home/ubuntu/server/build /home/ubuntu/server/lastbuild
+/rm -r /home/ubuntu/server/lastbuild/
+/mv /home/ubuntu/server/build /home/ubuntu/server/lastbuild/
