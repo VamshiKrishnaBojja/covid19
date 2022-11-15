dd9a539471a1479c825fb8e48e77d1ec80c58068
Vamshi Krishna Bojja <114059960+VamshiKrishnaBojja@users.noreply.github.com>
2022-11-15 13:40:40 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/server
-curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
-apt -y install nodejs
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+yum -y install nodejs npm
